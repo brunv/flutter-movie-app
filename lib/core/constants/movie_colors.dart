22b04b9bd103610beba30bb99069fb7be9121160
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2023-02-28 09:08:01 -0300

    Create custom color constants

---
+++ lib/core/constants/movie_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class MovieColors {
+  static const white = Color.fromARGB(255, 255, 255, 255);
+
+  static const blue1 = Color.fromARGB(255, 17, 14, 71);
+  static const blue2 = Color.fromARGB(255, 136, 165, 232);
+  static const blue3 = Color.fromARGB(255, 219, 227, 255);
+
+  static const gray1 = Color.fromARGB(255, 188, 188, 205);
+  static const gray2 = Color.fromARGB(255, 216, 216, 216);
+}
